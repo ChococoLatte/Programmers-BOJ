47d03705db414afe6b8fe503492a160afa8edc40
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-10-21 20:48:30 +0900

    [Silver V] Title: 단어 정렬, Time: 232 ms, Memory: 28940 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,38 +2,28 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static class Node implements Comparable<Node>{
-        int length;
-        String word;
-
-        public Node(int length, String word){
-            this.length = length;
-            this.word = word;
-        }
-
-        @Override
-        public int compareTo(Node n){
-            if(this.length == n.length) return this.word.compareTo(n.word);
-            return this.length-n.length;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<Node> list = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
         for(int i=0;i<n;i++){
-            String s = br.readLine();
-
-            list.add(new Node(s.length(),s));
+            arr[i] = br.readLine();
         }
 
-        Collections.sort(list);
+        Arrays.sort(arr, new Comparator<String>(){
+            public int compare(String o1, String o2){
+                if(o1.length() == o2.length()) return o1.compareTo(o2);
+                return o1.length()-o2.length();
+            }
+        });
+
         StringBuilder sb = new StringBuilder();
-        sb.append(list.get(0).word).append("\n");
-        for(int i=1;i<list.size();i++){
-            if(!list.get(i-1).word.equals(list.get(i).word)) sb.append(list.get(i).word).append("\n");
+        sb.append(arr[0]).append("\n");
+        for(int i=1;i<arr.length;i++){
+            if(!arr[i-1].equals(arr[i])) sb.append(arr[i]).append("\n");
         }
+
         System.out.println(sb);
     }
 }
