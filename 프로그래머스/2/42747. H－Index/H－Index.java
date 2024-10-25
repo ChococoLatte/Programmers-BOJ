beaaee22dddb1a0a47540983ad43d8e678282839
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-10-25 23:26:20 +0900

    [level 2] Title: H-Index, Time: 1.82 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        
+        int max = 0;
+        for(int i=0;i<citations.length;i++){
+            int h = citations.length-i;
+            
+            if(citations[i]>=h){
+                max = h;
+                break;
+            }
+        }
+        
+        return max;
+    }
+}
