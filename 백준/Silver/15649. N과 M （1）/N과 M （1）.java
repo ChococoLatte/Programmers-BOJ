40698e0b12e91e93b2d8ead710c4c1961d48d165
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-02-02 14:52:23 +0700

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 21380 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int N,M;
-	static int[] arr,ans;
-	static boolean[] isSelected;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		ans = new int[M];
-		isSelected =new boolean[N];
-		
-		perm(0);
-		
-	}
-	
-	private static void perm(int idx) {
-		
-		if(idx==M) {
-			for(int i:ans) System.out.print(i+" ");
-			System.out.println();
-			return;
-		}
-		
-		for(int i=0;i<N;i++) {
-			
-			if(isSelected[i]) continue;
-			
-			isSelected[i] = true;
-			ans[idx] = i+1;
-			perm(idx+1);
-			isSelected[i] = false;
-		}
-	}
+class Main {
+    static int N,M;
+    static int[] num;
+    static boolean[] isSelected;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[M];
+        isSelected = new boolean[N];
+
+        sb = new StringBuilder();
+        perm(0);
+
+        System.out.println(sb);
+        
+    }
+
+    static void perm(int cnt){
+
+        if(cnt == M){
+            for(int nm:num){
+                sb.append(nm).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            if(isSelected[i]) continue;
+
+            isSelected[i] = true;
+            num[cnt] = i+1;
+            perm(cnt+1);
+            isSelected[i] = false;
+        }
+    }
 }
