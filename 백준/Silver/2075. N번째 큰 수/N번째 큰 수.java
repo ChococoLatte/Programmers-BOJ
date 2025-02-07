ecf6f8e2b2961805d2d7909fafec0d6331205050
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-02-07 15:53:12 +0700

    [Silver III] Title: N번째 큰 수, Time: 860 ms, Memory: 297996 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -2,34 +2,42 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static class Point implements Comparable<Point>{
+    static class Point implements Comparable<Point> {
         int val;
 
-        public Point(int val){
+        public Point(int val) {
             this.val = val;
         }
 
         @Override
-        public int compareTo(Point p){
-            return p.val-this.val;
+        public int compareTo(Point p) {
+            return this.val - p.val;  // 오름차순으로 정렬
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         
-        ArrayList<Point> list = new ArrayList<>();
-        for(int i=0;i<N;i++){
+        // 최소 힙 (PriorityQueue)으로 초기화
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++){
+            for (int j = 0; j < N; j++) {
                 int v = Integer.parseInt(st.nextToken());
-                list.add(new Point(v));
+                pq.offer(new Point(v));  // 우선순위 큐에 값 삽입
+                
+                // 큐의 크기가 N보다 커지면 가장 작은 값 삭제
+                if (pq.size() > N) {
+                    pq.poll();
+                }
             }
         }
 
-        Collections.sort(list);
-        System.out.println(list.get(N-1).val);
+        // N번째로 큰 값은 최소 힙에서 가장 작은 값
+        System.out.println(pq.poll().val);
     }
 }
