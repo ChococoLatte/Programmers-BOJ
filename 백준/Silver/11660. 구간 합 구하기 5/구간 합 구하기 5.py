31c0e86fbfe5a62834e7bee1804a2ff90cdf0cf6
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2026-03-11 20:47:05 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 948 ms, Memory: 73392 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,15 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N,M = map(int, input().split())
-
 arr = [[0]*(N+1) for _ in range(N+1)]
-for i in range(1,N+1):
+
+for i in range(1, N+1):
     row = list(map(int, input().split()))
-    for j in range(1,N+1):
+    for j in range(1, N+1):
         arr[i][j] = row[j-1]+arr[i-1][j]+arr[i][j-1]-arr[i-1][j-1]
 
 for _ in range(M):
     A,B,C,D = map(int, input().split())
-    print(arr[C][D]-arr[A-1][D]-arr[C][B-1]+arr[A-1][B-1])
-    
-        
+    tot = arr[C][D]-arr[A-1][D]-arr[C][B-1]+arr[A-1][B-1]
+    print(tot)
