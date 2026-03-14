382a9b19c7636ca7658954375eb566304ca13b9d
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2026-03-14 10:39:24 +0900

    [Silver V] Title: 수들의 합 5, Time: 4408 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -10,19 +10,14 @@ ans = 1
 
 while right < N:
     if sum < N:
-        right += 1
+        right +=1
         sum += right
     elif sum > N:
         sum -= left
-        left += 1
+        left +=1
     else:
-        ans += 1
         right += 1
         sum += right
-        
-print(ans)
-        
-    
+        ans +=1
 
-        
-    
+print(ans)
